08ca20a0a04fd310a8283d6a4558e215d5833a98
Romain Gréaume <r.greaume@beapp.fr>
2022-05-02 20:55:04 +0200

    feat: add mimeType search function & mimetype check pattern

--- lib/mimecon.dart
+++ lib/mimecon.dart
@@ -1,7 +1,121 @@
 library mimecon;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import 'package:mimecon/mimetype.dart';
+import 'dart:developer' as devlog;
+
+class Mimecon extends StatefulWidget {
+  final String mimetype;
+  final Color? color;
+  final double? size;
+  Mimecon({
+    Key? key,
+    required this.mimetype,
+    this.color,
+    this.size,
+  })  : assert(mimetype.isNotEmpty),
+        super(key: key);
+
+  @override
+  State<Mimecon> createState() => _MimeconState();
+}
+
+class _MimeconState extends State<Mimecon> {
+  @override
+  void initState() {
+    super.initState();
+    catchError(widget.mimetype);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _setIcon(widget.mimetype, widget.color, widget.size);
+  }
+
+  void catchError(String mimetype) {
+    RegExp mimetypePattern = RegExp(r'^[-\w.]+/[-\w.]+$');
+    if (!mimetypePattern.hasMatch(mimetype)) {
+      devlog.log("the entered mimetype does not correspond to a standard mimetype format");
+    }
+  }
+
+  Icon _setIcon(String mimeType, Color? color, double? size) {
+    var iconColor = color ?? const Color(0xFF858593);
+    var iconSize = size ?? 20.0;
+    if (imageMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.image,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (videoMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.filmstripBox,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (audioMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.musicBox,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (docMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.microsoftWord,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (excelMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.microsoftExcel,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (powerpointMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.microsoftPowerpoint,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (googleDriveMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.googleDrive,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (pdfMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.filePdfBox,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (libreOfficeWriterMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.fileDocument,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (libreOfficeImpressMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.presentation,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else if (libreOfficeCalcMimeType.contains(mimeType)) {
+      return Icon(
+        MdiIcons.fileTable,
+        color: iconColor,
+        size: iconSize,
+      );
+    } else {
+      return Icon(
+        MdiIcons.fileQuestion,
+        color: iconColor,
+        size: iconSize,
+      );
+    }
+  }
 }
---
+++ lib/mimetype.dart
@@ -0,0 +1,119 @@
+List<String> imageMimeType = [
+  "image/gif",
+  "image/jpg",
+  "image/jpeg",
+  "image/png",
+  "image/x-png",
+  "image/tiff",
+  "image/bmp",
+  "image/svg+xml",
+  "image/webp",
+];
+
+List<String> videoMimeType = [
+  "application/x-flash-video",
+  "application/x-shockwave-flash",
+  "video/mp4",
+  "video/webm",
+  "video/x-ms-asf",
+  "video/x-msvideo",
+  "video/x-matroska",
+  "video/quicktime",
+  "video/mpeg",
+  "video/x-ms-wmv",
+];
+
+List<String> audioMimeType = [
+  "audio/mp3",
+  "audio/mpeg",
+  "audio/mpeg3",
+  "audio/x-mpeg-3",
+  "audio/ogg",
+  "audio/x-vorbis+ogg",
+  "audio/mp4",
+  "audio/x-wav",
+  "audio/x-ms-wma",
+];
+
+List<String> docMimeType = [
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  "application/msword",
+  "application/vnd.ms-word",
+  "application/doc",
+  "application/text",
+  "application/vnd.msword",
+  "application/winword",
+  "application/word",
+  "application/x-msw6",
+  "application/x-msword",
+];
+
+List<String> excelMimeType = [
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  "application/vnd.ms-excel",
+  "application/msexcel",
+  "application/x-msexcel",
+  "application/x-ms-excel",
+  "application/x-excel",
+  "application/x-dos_ms_excel",
+  "application/xls",
+];
+
+List<String> powerpointMimeType = [
+  "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+  "application/vnd.ms-powerpoint",
+  "application/mspowerpoint",
+  "application/ms-powerpoint",
+  "application/mspowerpnt",
+  "application/vnd-mspowerpoint",
+  "application/powerpoint",
+  "application/x-powerpoint",
+  "application/x-m",
+];
+
+List<String> googleDriveMimeType = [
+  "application/vnd.google-apps.document",
+  "application/vnd.google-apps.spreadsheet",
+  "application/vnd.google-apps.presentation",
+];
+
+List<String> pdfMimeType = [
+  "application/pdf",
+  "application/x-pdf",
+  "application/acrobat",
+  "application/vnd.pdf",
+  "text/pdf",
+  "text/x-pdf",
+];
+
+List<String> libreOfficeWriterMimeType = [
+  "application/vnd.sun.xml.writer",
+  "application/vnd.oasis.opendocument.text",
+  "application/application/x-vnd.oasis.opendocument.text",
+];
+
+List<String> libreOfficeImpressMimeType = [
+  "application/vnd.sun.xml.impress",
+  "application/vnd.oasis.opendocument.presentation",
+  "application/x-vnd.oasis.opendocument.presentation",
+];
+
+List<String> libreOfficeCalcMimeType = [
+  "application/vnd.sun.xml.calc",
+  "application/vnd.oasis.opendocument.spreadsheet",
+  "application/x-vnd.oasis.opendocument.spreadsheet",
+];
+
+List<String> genericMimeType = [
+  "text/plain",
+  "application/octet-stream",
+  "application/zip",
+  "application/x-zip-compressed",
+  "application/x-tar-gz",
+  "text/csv",
+  "text/vtt",
+  "application/xml",
+  "application/json",
+  "application/msword",
+  "application/mswrite",
+];
